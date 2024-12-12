05d00b0a8d4aa75a0b1aed4948d315e2c15db948
sherlynkek <sherlynkek00@gmail.com>
2024-12-12 12:25:32 +0800

    minor edit

--- src/main/java/sg/edu/nus/iss/vttp5a_day9l/constant/Constant.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_day9l/constant/Constant.java
@@ -2,5 +2,5 @@ package sg.edu.nus.iss.vttp5a_day9l.constant;
 
 public class Constant {
     public static final String weatherURl = "http://api.weatherapi.com/v1/current.json";
-    public static final String apiKey = "db403e8af1aa4ff296325345240612";
+    public static final String apiKey = "f90d5811caf64e93aa425804240612";
 }
